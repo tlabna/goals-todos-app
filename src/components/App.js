ae58d7a2ae74b77470daf82d6590c4879c3a9361
tlabna <tlabna@users.noreply.github.com>
2018-04-11 21:02:36 -0400

    refactor: Move components to components directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import {
+  handleInitialData
+} from '../actions/shared'
 
 
 class App extends Component {
+  componentDidMount () {
+    const { dispatch } = this.props
+
+    store.dispatch(handleInitialData())
+  }
   render() {
+    const { loading } = this.props
+    if (loading === true) {
+      return <h3>Loading</h3>
+    }
+
     return (
       <div>
-        Hello World!
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function List (props) {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item.id}>
+          <span
+            style={{textDecoration: item.complete ? 'line-through' : 'none'}}
+            onClick={() => props.toggle && props.toggle(item)}>
+            {item.name}
+          </span>
+          <button onClick={() => props.remove(item)}>X</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
